cc7c28817c73a8431a546f02fad94346dd8aa9bc
Lucian Oancea <lucian.oancea@itsmartsystems.eu>
2021-02-16 16:30:10 +0200

    Added search for products by category id , added new images and updatings

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,26 +1,31 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Product } from '../common/product';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
   private baseUrl = 'http://localhost:8080/api/products';
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    // need to build URL based on category id 
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
 }
 
-interface GetResponse{
-  _embedded:{
+interface GetResponse {
+  _embedded: {
     products: Product[];
   }
 }
