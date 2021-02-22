0c0feee741cc05cd51be1c158b59eec9650b3dc1
Lucian Oancea <lucian.oancea@itsmartsystems.eu>
2021-02-22 23:44:34 +0200

    Added okta dependency with my-app-config and login component

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+    oidc:{
+        clientId: '0oa88pzcrbQgusN9L5d6',
+        issuer: 'https://dev-91337945.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
