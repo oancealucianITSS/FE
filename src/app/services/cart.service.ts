036143e4a71086fc84ba76eac112bae6cf23bea3
Lucian Oancea <lucian.oancea@itsmartsystems.eu>
2021-02-23 21:35:40 +0200

    handling local storage for refresh browser and login to still save data for cart

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -12,7 +12,20 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() { }
+  // storage: Storage = sessionStorage; //here after we close the browser we lose the data from cart
+  storage: Storage = localStorage; //here after we close we stil have data from cart
+
+  constructor() {
+    //read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems'));
+
+    if(data != null){
+      this.cartItems = data;
+
+      //compute totals based on the data that is read from storage
+      this.computeCartTotals();
+    }
+   }
 
   addToCart(theCartItem: CartItem) {
     //check if we alredy have the item in our cart
@@ -61,6 +74,13 @@ export class CartService {
 
     //log cart data just for debugging
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    //persist cart data
+    this.persistCartItems();
+  }
+
+  persistCartItems(){
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
   logCartData(totalPriceValue: number, totalQuantityValue: number){
