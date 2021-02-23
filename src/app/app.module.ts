c36b2848b097b16c1c3646f2af7d769543e0846b
Lucian Oancea <lucian.oancea@itsmartsystems.eu>
2021-02-23 17:54:37 +0200

    Added source code for members page(protected page , only for users logged)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,13 +21,15 @@ import { LoginStatusComponent } from './components/login-status/login-status.com
 import{
   OKTA_CONFIG,
   OktaAuthModule,
-  OktaCallbackComponent
+  OktaCallbackComponent,
+  OktaAuthGuard
 } from '@okta/okta-angular';
 
 import myAppConfig from './config/my-app-config';
+import { MembersPageComponent } from './components/members-page/members-page.component';
 
 const oktaConfig = Object.assign({
-  onAuthRequired: (injector) => {
+  onAuthRequired: (oktaAuth, injector) => {
     const router = injector.get(Router);
 
     //redrect the user to your custo login page
@@ -36,6 +38,8 @@ const oktaConfig = Object.assign({
 }, myAppConfig.oidc);
 
 const routes: Routes = [
+  {path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard]},
+
   {path: 'login/callback', component: OktaCallbackComponent},
   {path: 'login', component: LoginComponent},
 
@@ -61,7 +65,8 @@ const routes: Routes = [
     CartDetailsComponent,
     CheckoutComponent,
     LoginComponent,
-    LoginStatusComponent
+    LoginStatusComponent,
+    MembersPageComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
